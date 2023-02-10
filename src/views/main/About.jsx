87050e91cca5b5230400650eacd7c0cb7cc975e4
Harvansh S Rathore <78679500+Harvansh-1234@users.noreply.github.com>
2023-02-11 04:46:13 +0530

    about page done

--- src/views/main/About.jsx
+++ src/views/main/About.jsx
@@ -3,14 +3,24 @@ import about from 'assets/img/about.png'
 import './CSS/about.css'
 function About() {
   return (
-    <div>
+    <div style={{position:"relative"}}>
         <div className='about'>
         <img src={about} alt="" />
       </div> 
       <div className='aboutContent'>
-    <h1>Campus Engine <span>.</span>
+    <h1>Student <span>.</span>
     </h1>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut magni necessitatibus illum animi quod! Beatae facilis sint blanditiis nulla non et quaerat illum, iste adipisci amet nostrum recusandae delectus molestias.</p>
+    <p>Empowers students to create job profiles, search, and apply for jobs and internships; instant job matching that takes their career to the next level.</p>
+   </div>
+   <div className='aboutContent1'>
+    <h1>Companies <span>.</span>
+    </h1>
+    <p>Create multiple job postings and get instant access to the entire students' database to match them based on skills, GPA, and certificates for the best hiring experience.</p>
+   </div>
+   <div className='aboutContent2'>
+    <h1>TNP<span>.</span>
+    </h1>
+    <p>Attract several companies and manage them from a centralized place; capture data and reports related to students and employers in a single click, securely.</p>
    </div>
     </div>
   )
