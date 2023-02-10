c72f68e0d540e9b4a4855dd3e15f3940be4cb470
dhruv360code <dhruvtiwari003@gmail.com>
2023-02-11 00:03:03 +0530

    userUpdate Repo added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,4 @@
-const { getUserById } = require("../repository/user.repo");
+const { getUserById, updateUserById } = require("../repository/user.repo");
 const {
   serverErrorResponse,
   notFoundResponse,
@@ -25,4 +25,28 @@ const userInfo = async (req, res) => {
   }
 };
 
-module.exports = { userInfo };
+// update userInfo controller
+const updateUserInfo = async (req, res) => {
+  try {
+    let [err, user] = await getUserById(req.userId);
+    if (err) {
+      console.log(`Error in get user by id: ${err.message}`);
+      return serverErrorResponse(res, err.message);
+    }
+    if (user.length === 0) {
+      return notFoundResponse(res, "User not found");
+    }
+    let [err1, user1] = await updateUserById(req.userId, req.body);
+    if (err1) {
+      console.log(`Error in update user by id: ${err1.message}`);
+      return serverErrorResponse(res, err1.message);
+    }
+    return successResponse(res, "User info updated", user1[0]);
+  } catch (err) {
+    console.log("error", err);
+    handle304(err.message, res);
+    serverErrorResponse(res, "Internal Server Error");
+  }
+};
+
+module.exports = { userInfo, updateUserInfo };
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { userInfo } = require("../controllers/user.controller");
+const { userInfo, updateUserInfo } = require("../controllers/user.controller");
 const { userValidate } = require("../middlewares/authmiddleware");
 const router = express.Router();
 
 router.get("/getUserInfo", userValidate, userInfo);
+router.post("/updateUserInfo", userValidate, updateUserInfo);
 
 module.exports = router;
