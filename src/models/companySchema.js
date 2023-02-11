b2b16f36e06c0c302f7aab107705039c99729655
dhruv360code <dhruvtiwari003@gmail.com>
2023-02-11 08:13:00 +0530

    schema addition

--- src/models/companySchema.js
+++ src/models/companySchema.js
@@ -1,36 +1,37 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // create company Schema
-const companySchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-            trim: true,
-        },
-        email: {
-            type: String,
-            required: true,
-            trim: true,
-            lowercase: true,
-            unique: true,
-        },
-        password: {
-            type: String,
-            required: true,
-            trim: true,
-            minlength: 7,
-        },
-        contactNo: {
-            type: Number,
-            required: true,
-            minlength: 10,
-            maxlength: 10
-        },
-        address:{
-            type: String,
-            required: true,
-            trim: true,
-        },
+const companySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 7,
+  },
+  contactNo: {
+    type: Number,
+    required: true,
+    minlength: 10,
+    maxlength: 10,
+  },
+  address: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+});
 
-        })
+const Company = mongoose.model("Company", companySchema);
+module.exports = Company;
