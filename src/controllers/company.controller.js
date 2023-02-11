c0180f3525388cb62cf5fa14e097daaf9ee3ecc8
dhruv360code <dhruvtiwari003@gmail.com>
2023-02-12 00:09:15 +0530

    company routes added

--- src/controllers/company.controller.js
+++ src/controllers/company.controller.js
@@ -1,4 +1,44 @@
+// create Job post
 
+const { createOffCampusJobPostRepo } = require("../repository/quiz.repo");
 
+const createJobPost = async (req, res) => {
+  try {
+    // create job post
+    let [err1, jobPost1] = await createOffCampusJobPostRepo(req.body);
+    if (err1) {
+      console.log(`Error in create job post: ${err1.message}`);
+      return serverErrorResponse(res, err1.message);
+    }
+    return successResponse(res, jobPost1, "Job post created");
+  } catch (err) {
+    console.log(err);
+    return serverErrorResponse(res, err.message);
+  }
+};
 
+// get job post
+const getJobPost = async (req, res) => {
+  try {
+    // get job post
+    let [err1, jobPost1] = await getJobPost({
+      title: req.body.title,
+    });
+    if (err1) {
+      console.log(`Error in get job post: ${err1.message}`);
 
+      return serverErrorResponse(res, err1.message);
+    }
+    if (jobPost1.length === 0)
+      return notFoundResponse(res, "Job post not found");
+
+    return successResponse(res, jobPost1, "Job post fetched");
+  } catch (err) {
+    let errObj = {
+      message: err.message,
+      status: 500,
+    };
+
+    return serverErrorResponse(res, errObj);
+  }
+};
